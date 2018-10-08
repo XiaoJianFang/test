226c66ad2cf6b121ae0d10825b39defe1eaad4b0
XiaoJianFang <beichenstar@126.com>
2018-10-08 07:03:15 -0400

    add osm test demo

---
+++ OsmTest/cmake-build-debug/CMakeFiles/DemoMap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoMap.dir/demo.cpp.o"
+  "DemoMap.pdb"
+  "DemoMap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemoMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
